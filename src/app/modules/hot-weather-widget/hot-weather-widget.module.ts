c083f047d05cb0de4be3b2c6050adcbb28267f02
Artik <mail@artik.me>
2020-02-06 22:26:55 +0300

    FIX: dirs

--- src/app/modules/hot-weather-widget/hot-weather-widget.module.ts
+++ src/app/modules/hot-weather-widget/hot-weather-widget.module.ts
@@ -2,9 +2,9 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HotWeatherWidgetMainComponent } from './hot-weather-widget-main/hot-weather-widget-main.component';
 import { RouterModule } from '@angular/router';
-import { HotWeatherWidgetHotelListComponent } from './hot-weather-widget-hotel-list/hot-weather-widget-hotel-list.component';
-import { HotWeatherWidgetWeatherComponent } from './hot-weather-widget-weather/hot-weather-widget-weather.component';
-import { HotWeatherWidgetSocialComponent } from './hot-weather-widget-social/hot-weather-widget-social.component';
+import { HotWeatherWidgetHotelListComponent } from './hot-weather-widget-main/hot-weather-widget-hotel-list/hot-weather-widget-hotel-list.component';
+import { HotWeatherWidgetWeatherComponent } from './hot-weather-widget-main/hot-weather-widget-weather/hot-weather-widget-weather.component';
+import { HotWeatherWidgetSocialComponent } from './hot-weather-widget-main/hot-weather-widget-social/hot-weather-widget-social.component';
 import { NgxMaskModule } from 'ngx-mask';
 
 @NgModule({
